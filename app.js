04923652d3fb217306eddc566c7778249fda0a9e
Arpit Shakya <145144967+shakyaarpit@users.noreply.github.com>
2023-10-21 16:24:15 +0530

    first commit

---
+++ app.js
@@ -0,0 +1,6 @@
+let barger = document.getElementById("barger");
+let navlist = document.getElementById("navlist");
+
+barger.addEventListener("click", function () {
+  navlist.classList.toggle("navlist-active");
+});
